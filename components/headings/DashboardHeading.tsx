cb1ccea23e459f66c7c17173ac7f1295c3e7dc50
Scott Block <scottrblock@gmail.com>
2022-01-30 00:14:10 -0800

    nav almost working

--- components/headings/DashboardHeading.tsx
+++ components/headings/DashboardHeading.tsx
@@ -1,18 +1,29 @@
-import { Heading, useColorModeValue } from "@chakra-ui/react";
+import { Box, Heading, useColorModeValue } from "@chakra-ui/react";
 import { printTitle } from "../sidebar/SidebarLink";
 
 type DashboardHeadingProps = {
+  activeTitle: string;
   title: string;
+  icon: React.ReactElement;
 };
 
+export const HEADING_CLASS = "dashboard-heading";
+
 export const DashboardHeading = (props: DashboardHeadingProps) => {
-  const { title } = props;
+  const { title, activeTitle, icon } = props;
+
+  const colors =
+    title === activeTitle
+      ? useColorModeValue("ptnGreen.700", "ptnGreen.300")
+      : useColorModeValue("blackAlpha.700", "whiteAlpha.700");
   return (
-    <Heading
-      fontSize={"2xl"}
-      color={useColorModeValue("blackAlpha.700", "whiteAlpha.700")}
-    >
-      {printTitle(title)}
-    </Heading>
+    <>
+      <Heading fontSize={"2xl"} color={colors} className={HEADING_CLASS}>
+        {printTitle(title)}
+      </Heading>
+      <Box fontSize="18" color={colors}>
+        {icon}
+      </Box>
+    </>
   );
 };
