a6aeb89169d8d5c7311d0bd435866a0cd769d3d2
Scott Block <scottrblock@gmail.com>
2022-05-24 21:16:14 -0400

    update pricing links

--- components/widgets/BillingWidget.tsx
+++ components/widgets/BillingWidget.tsx
@@ -24,7 +24,7 @@ export const BillingWdiget = (props: BillingWidgetProps) => {
   const { user, subscriptionLoading, stripeData } = props;
 
   const has_subscription =
-    stripeData && stripeData?.status?.includes("expired");
+    stripeData && !stripeData?.status?.includes("expired");
 
   return (
     <>
