b78e2822ada6a6fcb8ebc965428ed4e79c72fc49
Scott Block <scottrblock@gmail.com>
2022-05-26 00:10:37 -0400

    send params to stripe for redirect

--- components/areas/SignedOutArea.tsx
+++ components/areas/SignedOutArea.tsx
@@ -1,12 +1,17 @@
 import { Box, Button, HStack } from "@chakra-ui/react";
 import { SignIn, SignInButton } from "@clerk/nextjs";
+import useSearchParams from "../../hooks/useParams";
 
 export const SignedOutArea = () => {
+  const searchParams = new URLSearchParams(`${window.location.search}`);
+
   return (
     <Box minH="100vh" p="4">
       <HStack>
         <SignInButton
-          redirectUrl={`${process.env.NEXT_PUBLIC_NEW_DASHBOARD_SITE}`}
+          redirectUrl={`${process.env.NEXT_PUBLIC_NEW_DASHBOARD_SITE}/${
+            window.location.pathname
+          }?${searchParams.toString()}`}
         >
           <Button variant="ptn">sign in</Button>
         </SignInButton>
