bb9572653103946fdfb1ce7c90a7bb513bf6e247
Scott Block <scottrblock@gmail.com>
2022-07-19 19:58:17 -0400

    error boundary

---
+++ components/ErrorBoundary.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+class ErrorBoundary extends React.Component {
+  constructor(props: {} | Readonly<{}>) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(error: any) {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error: any, errorInfo: any) {
+    // TODO send to sentry
+  }
+
+  render() {
+    const { hasError } = this.state as any;
+
+    if (hasError) {
+      // You can render any custom fallback UI
+      return <h1>Something went wrong.</h1>;
+    }
+
+    return this.props.children;
+  }
+}
+
+export { ErrorBoundary };
