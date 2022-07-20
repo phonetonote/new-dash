32b6ff89170974cadc4859c5897bc8e4b158c1b4
Scott Block <scottrblock@gmail.com>
2022-07-19 20:25:12 -0400

    sentry cleanup

--- sentry.client.config.js
+++ sentry.client.config.js
@@ -2,12 +2,12 @@
 // The config you add here will be used whenever a page is visited.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
-import * as Sentry from '@sentry/nextjs';
+import * as Sentry from "@sentry/nextjs";
 
 const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
 
 Sentry.init({
-  dsn: SENTRY_DSN || 'https://3e1a39cda10643e3889b94fa92717798@o1326025.ingest.sentry.io/6585766',
+  dsn: SENTRY_DSN,
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1.0,
   // ...
