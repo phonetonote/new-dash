7d400c9e64d8c185764ec8b5b53cf7cac0d81e4c
Scott Block <scottrblock@gmail.com>
2022-07-21 21:05:30 -0400

    cleanup

--- sentry.server.config.js
+++ sentry.server.config.js
@@ -7,7 +7,7 @@ import * as Sentry from '@sentry/nextjs';
 const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
 
 Sentry.init({
-  dsn: SENTRY_DSN || 'https://3e1a39cda10643e3889b94fa92717798@o1326025.ingest.sentry.io/6585766',
+  dsn: SENTRY_DSN,
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1.0,
   // ...
