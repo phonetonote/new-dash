b3625c4d34ef6a84e4af67be2034096a130cc43d
Scott Block <scottrblock@gmail.com>
2022-07-24 11:25:36 -0400

    Fix the welcome page to handle existing sessions

--- pages/welcome.tsx
+++ pages/welcome.tsx
@@ -3,66 +3,92 @@ import {
   GetServerSideProps,
   NextPage,
 } from "next";
-import { useUser, useSignIn } from "@clerk/nextjs";
+import { useSignIn, useClerk } from "@clerk/nextjs";
 import { useEffect, useState } from "react";
 import { PageWithLayout } from "../types/PageWithLayout";
-import WithSidebar from "../components/layouts/WithSidebar";
-import WithTopbar from "../components/layouts/WithTopbar";
 import Router from "next/router";
-import { SignedOutArea } from "../components/areas/SignedOutArea";
 import EmptyPage from "../components/layouts/EmptyPage";
-import { Spinner } from "@chakra-ui/react";
 
 // Grab the query param server side, and pass through props
 export const getServerSideProps: GetServerSideProps = async (context) => {
+  const {
+    query: { token: signInToken, clerkIdFromRoam },
+  } = context;
   return {
-    props: { signInToken: context.query.token ? context.query.token : null },
+    props: {
+      signInToken,
+      clerkIdFromRoam,
+    },
   };
 };
 
 const Welcome: NextPage = ({
   signInToken,
+  clerkIdFromRoam: clerkId,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+  const { signOut } = useClerk();
   const { signIn, setSession } = useSignIn();
-  const { isSignedIn } = useUser();
-  const [signInStatus, setSignInStatus] = useState<string>("WAITING");
+  const { session } = useClerk();
+  const [mySessionId, setMySessionId] = useState<string>();
+  const [goToRedirect, setGoToRedirect] = useState<boolean>(false);
 
   useEffect(() => {
-    if (isSignedIn) {
-      setSignInStatus("COMPLETE");
-      return;
-    }
-    if (!signIn || !signInToken || !setSession) {
-      setSignInStatus("ERROR");
+    if (!signIn || !setSession || !signInToken || !signOut) {
       return;
     }
 
-    const aFunc = async () => {
-      const res = await signIn.create({
-        strategy: "ticket",
-        ticket: signInToken as string,
-      });
+    const determineSession = async () => {
+      const prepareToSignIn = async () => {
+        const { createdSessionId } = await signIn.create({
+          strategy: "ticket",
+          ticket: signInToken as string,
+        });
 
-      setSession(res.createdSessionId, () => {
-        setSignInStatus("COMPLETE");
-      });
+        setMySessionId(createdSessionId as string);
+      };
+      try {
+        if (session) {
+          const {
+            user: { id: sessionUserId },
+          } = session;
+          const hasClerkId = clerkId !== "null";
+          const sessionIsValid = hasClerkId && sessionUserId === clerkId;
+
+          if (!hasClerkId || (hasClerkId && sessionIsValid)) {
+            setGoToRedirect(true);
+          } else if (hasClerkId && !sessionIsValid) {
+            await signOut();
+            await prepareToSignIn();
+          } else {
+            console.error(
+              "error state, please contact support@phonetonote.com"
+            );
+          }
+        } else {
+          await prepareToSignIn();
+        }
+      } catch (err) {
+        console.error(err);
+      }
     };
 
-    aFunc();
-  }, [signIn, signInToken, isSignedIn, setSession]);
+    determineSession();
+  }, [session, signInToken, clerkId, signIn, signOut, setSession]);
 
   useEffect(() => {
-    if (signInStatus === "COMPLETE") {
+    if (mySessionId && setSession && !goToRedirect) {
+      setSession(mySessionId);
+      setGoToRedirect(true);
+    }
+  }, [mySessionId, setSession, setGoToRedirect, goToRedirect]);
+
+  useEffect(() => {
+    if (goToRedirect) {
       Router.push("/");
     }
-  }, [signInStatus]);
+  }, [goToRedirect]);
 
-  return (
-    <div>
-      {signInStatus === "WAITING" && <Spinner />}
-      {signInStatus === "ERROR" && <SignedOutArea />}
-    </div>
-  );
+  return <div>loading...</div>;
 };
 
 (Welcome as PageWithLayout).layout = EmptyPage;
