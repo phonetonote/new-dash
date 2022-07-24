7fcb0465e3bb093e764d4bcb84e8c47391a6a9dc
Scott Block <scottrblock@gmail.com>
2022-07-24 11:55:01 -0400

    checking with a clerkid param

--- pages/welcome.tsx
+++ pages/welcome.tsx
@@ -15,25 +15,41 @@ import { Spinner } from "@chakra-ui/react";
 
 // Grab the query param server side, and pass through props
 export const getServerSideProps: GetServerSideProps = async (context) => {
+  const {
+    query: { token: signInToken, clerkIdFromRoam },
+  } = context;
   return {
-    props: { signInToken: context.query.token ? context.query.token : null },
+    props: {
+      signInToken,
+      clerkIdFromRoam,
+    },
   };
 };
 
 const Welcome: NextPage = ({
   signInToken,
+  clerkIdFromRoam,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
-  const { signIn, setSession } = useSignIn();
   const { signOut } = useClerk();
-  const { isSignedIn } = useUser();
+  const { signIn, setSession } = useSignIn();
+  const { isSignedIn, user } = useUser();
   const [signInStatus, setSignInStatus] = useState<string>("WAITING");
 
   useEffect(() => {
-    if (isSignedIn && setSession) {
-      signOut().then(() => {
-        setSignInStatus("RESTART");
-        return;
-      });
+    if (isSignedIn) {
+      console.log("signed in with user", user);
+
+      if (user && user.id && user.id !== clerkIdFromRoam) {
+        alert(
+          "you were previously logged into phonetonote with a different account. you are now logged out of the old one. please relick the dashboard link to sign in with the correct account."
+        );
+
+        signOut().then(() => {
+          setSignInStatus("COMPLETE");
+        });
+      }
+      setSignInStatus("COMPLETE");
+      return;
     }
     if (!signIn || !signInToken || !setSession) {
       setSignInStatus("ERROR");
@@ -52,17 +68,13 @@ const Welcome: NextPage = ({
     };
 
     aFunc();
-  }, [signIn, signInToken, isSignedIn, setSession, signOut]);
+  }, [signIn, signInToken, isSignedIn, setSession, user]);
 
   useEffect(() => {
     if (signInStatus === "COMPLETE") {
       Router.push("/");
     }
-
-    if (signInStatus === "RESTART") {
-      Router.push("/welcome?token=" + signInToken);
-    }
-  }, [signInStatus, signInToken]);
+  }, [signInStatus]);
 
   return (
     <div>
